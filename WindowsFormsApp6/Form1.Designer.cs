9a675ed7c76ebe101aff335c24d44ea7ba051ba4
My Name <kuchumovalex49@gmail.com>
2024-01-31 23:27:41 +0300

    intial commit

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(299, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 35);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Войти";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 289);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(211, 24);
+            this.textBox2.TabIndex = 15;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(299, 204);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(211, 26);
+            this.textBox1.TabIndex = 14;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Работник",
+            "Посетитель"});
+            this.comboBox1.Location = new System.Drawing.Point(299, 120);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(211, 24);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(367, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(367, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(329, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Выберите роль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(291, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 43);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Авторизация";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Vxod_Click(object sender, EventArgs e)
+        {
+            {
+                if (comboBox1.Text == "Посетитель" && textBox1.Text == "admin" && textBox2.Text != "")
+                    MessageBox.Show("У вас нет доступа!", "Ошибка");
+
+                {
+
+
+                    string connect = "data source=stud-mssql.sttec.yar.ru,38325;initial catalog=user52_db;persist security info=True;user id=user52_db;password=user52;MultipleActiveResultSets=True;App=EntityFramework";
+                    SqlConnection myConnection = new SqlConnection(connect);
+                    string command = "SELECT Login, Parol FROM KURS1_Posetitel WHERE Login = '" + textBox1.Text + "' AND Parol = '" + textBox2.Text + "'";
+                    myConnection.Open();
+                    SqlCommand myCommand = new SqlCommand(command, myConnection);
+                    SqlDataReader row;
+                    row = myCommand.ExecuteReader();
+                    string Login = "";
+                    string Parol = "";
+                    if (row.HasRows)
+                    {
+                        while (row.Read())
+                        {
+                            Login = row["Login"].ToString();
+                            Parol = row["Parol"].ToString();
+
+                        }
+
+                    }
+                    else if (comboBox1.Text == "Работник" && textBox1.Text != "" && textBox2.Text != "")
+
+                    {
+                        string connect1 = "data source=stud-mssql.sttec.yar.ru,38325;initial catalog=user52_db;persist security info=True;user id=user52_db;password=user52;MultipleActiveResultSets=True;App=EntityFramework";
+                        SqlConnection myConnection1 = new SqlConnection(connect1);
+                        string command1 = "SELECT Login1, Parol1 FROM KURS_Admin WHERE Login1 = '" + textBox1.Text + "' AND Parol1 = '" + textBox2.Text + "'";
+                        myConnection1.Open();
+                        SqlCommand myCommand1 = new SqlCommand(command1, myConnection1);
+                        SqlDataReader row1;
+                        row1 = myCommand1.ExecuteReader();
+                        string Login1 = "";
+                        string Parol1 = "";
+                        if (row1.HasRows)
+                        {
+                            while (row1.Read())
+                            {
+                                Login1 = row1["Login1"].ToString();
+                                Parol1 = row1["Parol1"].ToString();
+
+                            }
+
+
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+}
+
+            
